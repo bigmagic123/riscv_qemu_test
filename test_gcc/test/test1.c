e5da9454b64436bd85167d7d79eb49f3737e57a9
bigmagic <fujin.zhao@foxmail.com>
2021-07-27 09:50:55 +0800

    add test

--- test_gcc/test/test1.c
+++ test_gcc/test/test1.c
@@ -6,7 +6,30 @@ int main()
 {
     int64_t a, b, c;
    // c = __rv__ursub64(a, b);
-    c = __rv__kadd64(a, b);
+    c = __rv__dkhm8(a, b);
+    c = __rv__dkhm16(a, b);
+
+    c = __rv__dkabs8(a);
+    c = __rv__dkabs16(a);
+    //c = __rv__dkabs8(a);
+    //c = __rv__dkabs16(a);
+    c = __rv__kabs16(a);
+
+    c = __rv__dkslra8(a, b);
+    c = __rv__dkslra16(a, b);
+
+    c = __rv__dkadd8(a, b);
+    c = __rv__dkadd16(a, b);
+
+    c = __rv__dksub8(a, b);
+    c = __rv__dksub16(a, b);
+
+    //c = __rv__expd80(a);
+    //c = __rv__expd81(a);
+    //c = __rv__expd82(a);
+    //c = __rv__expd83(a);
+
+
     printf("hello world!\n");
     return 0;
 }
